4b9eb665a0e847acb1f1326f0c3a9cac31cb88b6
andresmorales8 <andresmorales62@gmail.com>
2019-11-12 19:14:03 -0500

    Agrega Proyecto Unity
    Se agrega el proyecto Unity con los scripts del taller de FPS.

---
+++ FPS camara/Assets/FPSMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMove : MonoBehaviour
+{
+    private Transform myTransform;
+    public float speed = 1f;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+        myTransform = GetComponent<Transform>();
+
+    }
+    
+    
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            myTransform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myTransform.Translate(new Vector3(0, 0, -speed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myTransform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myTransform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        }
+
+    }
+}
---
+++ FPS camara/Assets/FPSRotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSRotation : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+    float mouseX;
+    float mouseY;
+    public bool invertedMouse;
+    public GameObject body;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        mouseX += Input.GetAxis("Mouse X");
+        if (invertedMouse)
+
+        {
+
+            mouseY += Input.GetAxis("Mouse Y");
+        }
+
+        else
+
+        {
+
+            mouseY -= Input.GetAxis("Mouse Y");
+
+        }
+
+        Debug.Log("X" + mouseX + " y Y "+ mouseY);
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+        body.transform.eulerAngles = new Vector3(0,mouseX,0);
+
+    }
+}
